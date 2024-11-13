aeaee7b4cd2147d44c98657a9bd5031365c85992
junior31064049@gmail.com <junior31064049@gmail.com>
2024-11-13 06:37:30 -0500

    remove react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    robotsTxt(),
-    react({
-      include: ["**/react/*"],
-    }),
-  ],
+  integrations: [tailwind(), robotsTxt()],
   site: "https://porfolio.dev/",
 });
