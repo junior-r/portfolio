7583ce93159810905dff55fba5b64836e36bb12d
Junior R <junior31064049@gmail.com>
2026-05-16 17:11:07 -0500

    feat: add new cert

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -2,6 +2,16 @@ import type { Certificate } from "@/types/certificate";
 
 export const certificates: Certificate[] = [
   {
+    title: "Arquitecturas para Android con Kotlin, curso avanzado",
+    issuer: "Udemy",
+    date: "2026-05",
+    imageUrl:
+        "https://res.cloudinary.com/dwrdsepas/image/upload/f_auto,q_auto/v1778969059/Kotlin_Architectures_bxeqwu.jpg",
+    validationUrl:
+        "https://www.udemy.com/certificate/UC-0b11906f-e26a-4662-924b-545ed0b6e6e7/",
+    language: "Spanish",
+  },
+  {
     title: "Kotlin 2025: The Mega Course – All-in-One",
     issuer: "Udemy",
     date: "2026-03",
